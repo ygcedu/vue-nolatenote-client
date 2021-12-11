6e75d46393afeefcded143871cf026c88a26e9ea
ygcedu <937601471@qq.com>
2021-12-11 13:03:30 +0800

    新增路由和页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from '@/components/Login';
+import Notebooks from '@/components/NotebookList';
+import Note from '@/components/NoteDetail';
+import Trash from '@/components/TrashDetail';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/notebooks',
+      component: Notebooks
+    },
+    {
+      path: '/note/:noteId',
+      component: Note
+    },
+    {
+      path: '/trash/:noteId',
+      component: Trash
     }
   ]
 })
