ef737de0941b3ae6407d64ef7c490d4a2ec813ee
ygcedu <937601471@qq.com>
2021-12-13 20:10:38 +0800

    完成笔记添加功能

--- src/components/NoteSidebar.vue
+++ src/components/NoteSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="note-sidebar">
-    <span class="btn add-note">添加笔记</span>
+    <span class="btn add-note" @click="addNote">添加笔记</span>
     <el-dropdown class="notebook-title" @command="handleCommand" placement="bottom">
       <span class="el-dropdown-link">
        {{ curBook.title }}<i class="iconfont icon-down"></i>
@@ -64,6 +64,13 @@ export default {
         .then(res => {
           this.notes = res.data;
           this.$emit('update:notes', this.notes);
+        });
+    },
+    addNote() {
+      Notes.addNote({notebookId: this.curBook.id})
+        .then(res => {
+          console.log(res);
+          this.notes.unshift(res.data);
         });
     }
   }
