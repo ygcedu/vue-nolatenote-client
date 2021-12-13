236030eb9f3f0c6fe9b503d13ead10ea8fc97954
ygcedu <937601471@qq.com>
2021-12-13 19:53:19 +0800

    完成笔记标题和内容更新时自动保存功能

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -7,15 +7,19 @@
         <div class="note-bar">
           <span> 创建日期: {{ curNote.createdAtFriendly }}</span>
           <span> 更新日期: {{ curNote.updatedAtFriendly }}</span>
-          <span> {{ curNote.statusText }}</span>
+          <span> {{ statusText }}</span>
           <span class="iconfont icon-delete"></span>
           <span class="iconfont icon-fullscreen"></span>
         </div>
         <div class="note-title">
-          <input type="text" v-model:value="curNote.title" placeholder="输入标题">
+          <input type="text" v-model:value="curNote.title" @input="updateNote"
+                 @keydown="statusText='正在输入...'"
+                 placeholder="输入标题">
         </div>
         <div class="editor">
-          <textarea v-show="true" :value="curNote.content" placeholder="输入内容, 支持 markdown 语法"></textarea>
+          <textarea v-show="true" v-model:value="curNote.content" @input="updateNote"
+                    @keydown="statusText='正在输入...'"
+                    placeholder="输入内容, 支持 markdown 语法"></textarea>
           <div class="preview markdown-body" v-html="" v-show="false">
           </div>
         </div>
@@ -28,13 +32,16 @@
 import Auth from '@/apis/auth';
 import NoteSidebar from '@/components/NoteSidebar';
 import Bus from '@/helpers/bus';
+import _ from 'lodash';
+import Notes from '@/apis/notes';
 
 export default {
   components: {NoteSidebar},
   data() {
     return {
       curNote: {},
-      notes: []
+      notes: [],
+      statusText: '笔记未改动'
     };
   },
   created() {
@@ -47,6 +54,18 @@ export default {
     Bus.$on('update:notes', val => {
       this.curNote = val.find(note => note.id == this.$route.query.noteId) || {};
     });
+  },
+  methods: {
+    // 防抖
+    updateNote: _.debounce(function () {
+      Notes.updateNote({noteId: this.curNote.id},
+        {title: this.curNote.title, content: this.curNote.content})
+        .then(data => {
+          this.statusText = '已保存';
+        }).catch(data => {
+        this.statusText = '保存出错';
+      });
+    }, 300)
   },
   beforeRouteUpdate(to, from, next) {
     this.curNote = this.notes.find(note => note.id == to.query.noteId) || {};
