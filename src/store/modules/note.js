873a2b3530f9a92700f3fcfe4eaf848f494774f1
ygcedu <937601471@qq.com>
2021-12-13 23:30:55 +0800

    vuex 改造 NoteSidebar

--- src/store/modules/note.js
+++ src/store/modules/note.js
@@ -1,10 +1,68 @@
-const state = {};
+import Note from '@/apis/notes';
+import {Message} from 'element-ui';
 
-const getters = {};
+const state = {
+  notes: [],
+  curNote: {}
+};
 
-const mutations = {};
+const getters = {
+  notes: state => state.notes
+};
 
-const actions = {};
+const mutations = {
+  setNote(state, payload) {
+    state.notes = payload.notes;
+  },
+
+  addNote(state, payload) {
+    state.notes.unshift(payload.note);
+  },
+
+  updateNote(state, payload) {
+    let note = state.notes.find(note => note.id === payload.noteId) || {};
+    note.title = payload.title;
+    note.content = payload.content;
+  },
+
+  deleteNote(state, payload) {
+    state.notes = state.notes.filter(note => note.id !== payload.noteId);
+  }
+};
+
+const actions = {
+  getNotes({commit}, {notebookId}) {
+    Note.getAll({notebookId})
+      .then(res => {
+        commit('setNote', {notes: res.data});
+      });
+  },
+
+  addNote({commit}, {notebookId, title, content}) {
+    Note.addNote({notebookId}, {title, content})
+      .then(res => {
+        console.log('add success...', res);
+        commit('addNote', {note: res.data});
+        Message.success(res.msg);
+      });
+  },
+
+  updateNote({commit}, {noteId, title, content}) {
+    Notebook.updateNote({noteId}, {title, content})
+      .then(res => {
+        commit('updateNote', {noteId, title, content});
+        Message.success(res.msg);
+      });
+  },
+
+  deleteNote({commit}, {noteId}) {
+    Notebook.deleteNotebook({noteId})
+      .then(res => {
+        commit('deleteNotebook', {noteId});
+        Message.success(res.msg);
+      });
+  }
+};
 
 export default {
   state,
--- src/store/modules/notebook.js
+++ src/store/modules/notebook.js
@@ -2,12 +2,19 @@ import Notebook from '@/apis/notebooks';
 import {Message} from 'element-ui';
 
 const state = {
-  notebooks: []
-};
+  notebooks: null,
+  curBookId: null
+}
 
 const getters = {
-  notebooks: state => state.notebooks
-};
+  notebooks: state => state.notebooks || [],
+
+  curBook: state => {
+    if (!Array.isArray(state.notebooks)) return {};
+    if (!state.curBookId) return state.notebooks[0];
+    return state.notebooks.find(notebook => notebook.id == state.curBookId) || {};
+  }
+}
 
 const mutations = {
   setNotebooks(state, payload) {
@@ -19,25 +26,29 @@ const mutations = {
   },
 
   updateNotebook(state, payload) {
-    let notebook = state.notebooks.find(notebook => notebook.id === payload.notebookId) || {};
+    let notebook = state.notebooks.find(notebook => notebook.id == payload.notebookId) || {};
     notebook.title = payload.title;
   },
 
   deleteNotebook(state, payload) {
-    state.notebooks = state.notebooks.filter(notebook => notebook.id !== payload.notebookId);
+    state.notebooks = state.notebooks.filter(notebook => notebook.id != payload.notebookId);
+  },
+
+  setCurBook(state, payload) {
+    state.curBookId = payload.curBookId;
   }
-};
+}
 
 const actions = {
   getNotebooks({commit}) {
-    Notebook.getAll()
+    return Notebook.getAll()
       .then(res => {
         commit('setNotebooks', {notebooks: res.data});
       });
   },
 
   addNotebook({commit}, payload) {
-    Notebook.addNotebook({title: payload.title})
+    return Notebook.addNotebook({title: payload.title})
       .then(res => {
         console.log('add success...', res);
         commit('addNotebook', {notebook: res.data});
@@ -46,7 +57,7 @@ const actions = {
   },
 
   updateNotebook({commit}, payload) {
-    Notebook.updateNotebook(payload.notebookId, {title: payload.title})
+    return Notebook.updateNotebook(payload.notebookId, {title: payload.title})
       .then(res => {
         commit('updateNotebook', {notebookId: payload.notebookId, title: payload.title});
         Message.success(res.msg);
@@ -54,17 +65,17 @@ const actions = {
   },
 
   deleteNotebook({commit}, payload) {
-    Notebook.deleteNotebook(payload.notebookId)
+    return Notebook.deleteNotebook(payload.notebookId)
       .then(res => {
         commit('deleteNotebook', {notebookId: payload.notebookId});
         Message.success(res.msg);
       });
   }
-};
+}
 
 export default {
   state,
   getters,
   mutations,
   actions
-};
+}
